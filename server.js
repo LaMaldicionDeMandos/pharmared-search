49a77d8b71a8762f9bc099dc1169041d4346e250
Marcelo Pasut <pasutmarcelo@gmail.com>
2016-12-26 23:05:02 -0300

    pruebas de ingreso de entidad

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var es = require('elasticsearch');
 var esClient = new es.Client({host: config.elastic_host, log:'info'});
 var db = new (require('./services/storage'))(esClient);
 
+//PRUEBA
+var Entity = require('./model/model');
+db.addEntity(new Entity('saraza', 'Marcelo Pasut', 'Lavalleja 1745', Entity.EntityType.PHARMACIST));
+
 /* Routers */
 
 
